c5be693a8352dccfb9c72283d8e644895dca29c0
Puttu Pavan Kumar <101029891+Pavan1471@users.noreply.github.com>
2025-04-11 08:20:24 +0530

    Update vite.config.js

--- ads/vite.config.js
+++ ads/vite.config.js
@@ -1,16 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensure correct asset paths
+  base: './',
+  server: {
+    port: 3005, // <-- Assign your desired port here
+  },
 });
