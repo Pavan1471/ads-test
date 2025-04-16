0a651e3e3c1ebf99a2e619448d53ae0a8693b3d3
Puttu Pavan Kumar <101029891+Pavan1471@users.noreply.github.com>
2025-04-16 12:56:59 +0530

    Update vite.config.js

--- ads/vite.config.js
+++ ads/vite.config.js
@@ -1,14 +1,12 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import mkcert from 'vite-plugin-mkcert';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [
-    react(),     // React SWC plugin
-    mkcert()     // SSL for local dev
+    react()// React SWC plugin
+      // SSL for local dev
   ],
   publicDir: './public',
   server: {
